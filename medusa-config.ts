51a39362e6c0635cd9c36070f17e516c5a716536
lauter <chenqintao@salmonads.com>
2025-03-20 22:25:24 +0800

    first commit

---
+++ medusa-config.ts
@@ -0,0 +1,42 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  // modules: [
+  //   {
+  //     resolve: "@medusajs/medusa/locking",
+  //     options: {
+  //       providers: [
+  //         {
+  //           resolve: "@medusajs/medusa/locking-redis",
+  //           id: "locking-redis",
+  //           // set this if you want this provider to be used by default
+  //           // and you have other Locking Module Providers registered.
+  //           is_default: true,
+  //           options: {
+  //             redisUrl: process.env.REDIS_URL,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     resolve: "@medusajs/medusa/cache-redis",
+  //     options: {
+  //       redisUrl: process.env.REDIS_URL,
+  //     },
+  //   },
+  // ],
+
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  }
+})
